3c8440373a4184057e476d99f4bdea8d57c4bdcd
Aye Min Oo <ayeminoosc@gmail.com>
2025-08-03 13:57:43 +0800

    setup spring boot project

---
+++ src/test/java/com/acf/pos/restaurant/backend/RestaurantBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acf.pos.restaurant.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
